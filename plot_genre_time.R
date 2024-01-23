f025da62451e053511ef800364fe7d3fc4b7219c
Pieter Dolmans <p.c.m.dolmans@students.uu.nl>
2024-01-23 14:16:25 +0100

    Added genre to plot file, updated .gitignore

--- plot_genre_time.R
+++ plot_genre_time.R
@@ -1,16 +1,20 @@
 library(tidyverse)
 library(ggplot2)
 
-data <- read_csv('jazz_birthyears.csv') |>
-  filter(birthyear >= 1700)
+data <- read_csv('test.csv') |>
+  transform(Birthyear = as.numeric(Birthyear)) |>
+  filter(Birthyear >= 1700)
+generations <- read_csv('generations.csv')
 
 #not necessary right now, but might be for other plots
 musicians_per_year <- data |>
-  transform(birthyear = as.numeric(birthyear)) |>
-  group_by(birthyear) |>
+  transform(Birthyear = as.numeric(Birthyear)) |>
+  group_by(Birthyear) |>
   summarise(n = n()) |>
-  filter(birthyear >= 1700)
+  filter(Birthyear >= 1700)
 
 ggplot(data = data) +
-  aes(x = as.numeric(birthyear)) +
-  geom_line(stat = "bin", binwidth = 5)
+  aes(x = Birthyear, color = Genre) +
+  geom_line(stat = "bin", binwidth = 5) +
+  geom_vline(aes(xintercept = start_date), linetype = 'dashed', data = generations) +
+  geom_text(aes(x= start_date - 5, label= generation), y=180, colour="red", angle=90, data = generations)
