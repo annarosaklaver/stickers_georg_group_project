8db76b8c26a6f10a71f83c740214885d7cb4f4a9
Pieter Dolmans <p.c.m.dolmans@students.uu.nl>
2024-01-23 14:44:56 +0100

    added distinct

--- plot_genre_time.R
+++ plot_genre_time.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(ggplot2)
 
-data <- read_csv('test.csv') |>
+data <- read_csv('grouped_genre_and_birthyear.csv') |>
   transform(Birthyear = as.numeric(Birthyear)) |>
-  filter(Birthyear >= 1700)
+  filter(Birthyear >= 1850) |>
+  distinct()
 generations <- read_csv('generations.csv')
 
 #not necessary right now, but might be for other plots
@@ -11,10 +12,10 @@ musicians_per_year <- data |>
   transform(Birthyear = as.numeric(Birthyear)) |>
   group_by(Birthyear) |>
   summarise(n = n()) |>
-  filter(Birthyear >= 1700)
+  filter(Birthyear >= 1850)
 
 ggplot(data = data) +
   aes(x = Birthyear, color = Genre) +
   geom_line(stat = "bin", binwidth = 5) +
   geom_vline(aes(xintercept = start_date), linetype = 'dashed', data = generations) +
-  geom_text(aes(x= start_date - 5, label= generation), y=180, colour="red", angle=90, data = generations)
+  geom_text(aes(x= start_date - 5, label= generation), y=600, colour="red", angle=90, data = generations)
