f6237c0001fc47127efebc151943f7361dff24aa
Emma Davies <e.m.davies@students.uu.nl>
2024-01-24 16:30:10 +0100

    Plotted career start dates, updated birth years plots, uploaded all plots

--- R_scripts/plot_genre_career_start.R
+++ R_scripts/plot_genre_career_start.R
@@ -1,20 +1,19 @@
 library(tidyverse)
 library(ggplot2)
-library(dplyr)
 
-data <- read_csv('output_data/grouped_genre_and_birthyear.csv') |>
-  transform(Birthyear = as.numeric(Birthyear)) |>
-  filter(Birthyear >= 1850) |>
+data <- read_csv('output_data/grouped_genre_and_career_start.csv') |>
+  transform(Career_Start = as.numeric(Career_Start)) |>
+  filter(Career_Start >= 1850) |>
   distinct()
 generations <- read_csv('output_data/generations.csv')
 
 #not necessary right now, but might be for other plots
 musicians_per_year <- data |>
   transform(
-    Birthyear = as.numeric(Birthyear),
-    period = round(Birthyear / 5) * 5
+    Career_Start = as.numeric(Career_Start),
+    period = round(Career_Start / 5) * 5
   ) |>
-  filter(Birthyear >= 1900 & Birthyear <= 2000) |>
+  filter(Career_Start >= 1900) |>
   group_by(period, Genre) |>
   summarise(n = n()) |>
   group_by(period) |>
@@ -24,20 +23,20 @@ musicians_per_year <- data |>
 print(musicians_per_year)
 
 ggplot(data = data) +
-  aes(x = Birthyear, color = Genre) +
-  labs(x = 'Birth year', y = "Number of artists") +
+  aes(x = Career_Start, color = Genre) +
   geom_line(stat = "bin", binwidth = 5) +
+  labs(x = 'Start of career (year)', y = "Number of artists") +
   geom_vline(aes(xintercept = start_date), linetype = 'dashed', data = generations) +
-  geom_text(aes(x= start_date + 5, label= generation), y=750, hjust = 1, colour="red", angle=90, data = generations)
+  geom_text(aes(x= start_date + 5, label= generation), y=1165, hjust = 1, colour="red", angle=90, data = generations)
 
-ggsave('genre_birthyear_line.pdf', width = 20, height = 15, units = c('cm'))
+ggsave('career_start_line.pdf', width = 20, height = 15, units = c('cm'))
 
 ggplot(data = musicians_per_year) + 
   aes(x = period, y = percentage) + 
-  xlim(1900,2000) + 
-  labs(x = 'Birth year', y = "Percentage of artists") +
+  xlim(1920,2015) + 
+  labs(x = 'Start of career (year)', y = "Percentage of artists") +
   geom_area(aes(fill = Genre), alpha = 0.6, size = 0.5, colour = "black", stat = "identity")+ 
   geom_vline(aes(xintercept = start_date), linetype = 'dashed', data = generations) +
   geom_text(aes(x= start_date + 3, label= generation), y=2, hjust = 0, colour="white", angle=90, data = generations)
 
-ggsave('genre_birthyear_stacked.pdf', width = 20, height = 15, units = c('cm'))
+ggsave('career_start_stacked.pdf', width = 20, height = 15, units = c('cm'))
